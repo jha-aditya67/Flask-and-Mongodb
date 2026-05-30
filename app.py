d85d17c1555abdad6ce7a893ce8c253b49f36507
jha-aditya67 <adityajha6758@gmail.com>
2026-05-30 14:20:23 +0530

    corrected the app.py

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/api')
-def get_data():
+@app.route('/')
+def home():
+    return "Flask API is running"
 
+@app.route('/api', strict_slashes=False)
+def get_data():
     with open('data.json', 'r') as file:
         data = json.load(file)
 
